6d09186df5386f03405108977b349fa0a89e38f0
kyororay <puppulinkyoro@gmail.com>
2026-06-22 20:58:42 +0900

    変数シンボルの変更、不要個所の削除

--- ArcFormats/Edoire/ArcARC.cs
+++ ArcFormats/Edoire/ArcARC.cs
@@ -140,49 +140,6 @@ namespace GameRes.Formats.Edoire
         UnknownPlatform = 9999
     }
 
-    public enum GameType
-    {
-        Normal,
-        UnityCN,
-        GI,
-        GI_Pack,
-        GI_CB1,
-        GI_CB2,
-        GI_CB3,
-        GI_CB3Pre,
-        BH3,
-        BH3Pre,
-        BH3PrePre,
-        ZZZ_CB1,
-        SR_CB2,
-        SR,
-        TOT,
-        Naraka,
-        EnsembleStars,
-        OPFP,
-        FakeHeader,
-        FantasyOfWind,
-        ShiningNikki,
-        HelixWaltz2,
-        NetEase,
-        AnchorPanic,
-        DreamscapeAlbireo,
-        ImaginaryFest,
-        AliceGearAegis,
-        ProjectSekai,
-        CodenameJump,
-        GirlsFrontline,
-        Reverse1999,
-        ArknightsEndfield,
-        JJKPhantomParade,
-        MuvLuvDimensions,
-        PartyAnimals,
-        LoveAndDeepspace,
-        SchoolGirlStrikers,
-        ExAstris,
-        PerpetualNovelty,
-    }
-
     public class Header
     {
         public string signature;
@@ -247,14 +204,6 @@ namespace GameRes.Formats.Edoire
         public ulong m_RefTypeHash;
 
         public TypeTreeNode() { }
-
-        public TypeTreeNode(string type, string name, int level, bool align)
-        {
-            m_Type = type;
-            m_Name = name;
-            m_Level = level;
-            m_MetaFlag = align ? 0x4000 : 0;
-        }
     }
 
     public class ObjectInfo
@@ -267,157 +216,6 @@ namespace GameRes.Formats.Edoire
         public byte stripped;
         public long m_PathID;
         public SerializedType serializedType;
-    }
-
-    public class Game
-    {
-        public string Name { get; set; }
-        public GameType Type { get; }
-
-        public Game(GameType type)
-        {
-            Name = type.ToString();
-            Type = type;
-        }
-
-        public bool IsMhyGroup()
-        {
-            switch (Type)
-            {
-                case GameType.GI:
-                case GameType.GI_Pack:
-                case GameType.GI_CB1:
-                case GameType.GI_CB2:
-                case GameType.GI_CB3:
-                case GameType.GI_CB3Pre:
-                case GameType.BH3:
-                case GameType.BH3Pre:
-                case GameType.BH3PrePre:
-                case GameType.SR_CB2:
-                case GameType.SR:
-                case GameType.ZZZ_CB1:
-                case GameType.TOT:
-                    return true;
-                default:
-                    return false;
-            }
-        }
-    }
-
-    public static class CommonString
-    {
-        public static readonly Dictionary<uint, string> StringBuffer = new Dictionary<uint, string>
-        {
-            {0, "AABB"},
-            {5, "AnimationClip"},
-            {19, "AnimationCurve"},
-            {34, "AnimationState"},
-            {49, "Array"},
-            {55, "Base"},
-            {60, "BitField"},
-            {69, "bitset"},
-            {76, "bool"},
-            {81, "char"},
-            {86, "ColorRGBA"},
-            {96, "Component"},
-            {106, "data"},
-            {111, "deque"},
-            {117, "double"},
-            {124, "dynamic_array"},
-            {138, "FastPropertyName"},
-            {155, "first"},
-            {161, "float"},
-            {167, "Font"},
-            {172, "GameObject"},
-            {183, "Generic Mono"},
-            {196, "GradientNEW"},
-            {208, "GUID"},
-            {213, "GUIStyle"},
-            {222, "int"},
-            {226, "list"},
-            {231, "long long"},
-            {241, "map"},
-            {245, "Matrix4x4f"},
-            {256, "MdFour"},
-            {263, "MonoBehaviour"},
-            {277, "MonoScript"},
-            {288, "m_ByteSize"},
-            {299, "m_Curve"},
-            {307, "m_EditorClassIdentifier"},
-            {331, "m_EditorHideFlags"},
-            {349, "m_Enabled"},
-            {359, "m_ExtensionPtr"},
-            {374, "m_GameObject"},
-            {387, "m_Index"},
-            {395, "m_IsArray"},
-            {405, "m_IsStatic"},
-            {416, "m_MetaFlag"},
-            {427, "m_Name"},
-            {434, "m_ObjectHideFlags"},
-            {452, "m_PrefabInternal"},
-            {469, "m_PrefabParentObject"},
-            {490, "m_Script"},
-            {499, "m_StaticEditorFlags"},
-            {519, "m_Type"},
-            {526, "m_Version"},
-            {536, "Object"},
-            {543, "pair"},
-            {548, "PPtr<Component>"},
-            {564, "PPtr<GameObject>"},
-            {581, "PPtr<Material>"},
-            {596, "PPtr<MonoBehaviour>"},
-            {616, "PPtr<MonoScript>"},
-            {633, "PPtr<Object>"},
-            {646, "PPtr<Prefab>"},
-            {659, "PPtr<Sprite>"},
-            {672, "PPtr<TextAsset>"},
-            {688, "PPtr<Texture>"},
-            {702, "PPtr<Texture2D>"},
-            {718, "PPtr<Transform>"},
-            {734, "Prefab"},
-            {741, "Quaternionf"},
-            {753, "Rectf"},
-            {759, "RectInt"},
-            {767, "RectOffset"},
-            {778, "second"},
-            {785, "set"},
-            {789, "short"},
-            {795, "size"},
-            {800, "SInt16"},
-            {807, "SInt32"},
-            {814, "SInt64"},
-            {821, "SInt8"},
-            {827, "staticvector"},
-            {840, "string"},
-            {847, "TextAsset"},
-            {857, "TextMesh"},
-            {866, "Texture"},
-            {874, "Texture2D"},
-            {884, "Transform"},
-            {894, "TypelessData"},
-            {907, "UInt16"},
-            {914, "UInt32"},
-            {921, "UInt64"},
-            {928, "UInt8"},
-            {934, "unsigned int"},
-            {947, "unsigned long long"},
-            {966, "unsigned short"},
-            {981, "vector"},
-            {988, "Vector2f"},
-            {997, "Vector3f"},
-            {1006, "Vector4f"},
-            {1015, "m_ScriptingClassIdentifier"},
-            {1042, "Gradient"},
-            {1051, "Type*"},
-            {1057, "int2_storage"},
-            {1070, "int3_storage"},
-            {1083, "BoundsInt"},
-            {1093, "m_CorrespondingSourceObject"},
-            {1121, "m_PrefabInstance"},
-            {1138, "m_PrefabAsset"},
-            {1152, "FileSize"},
-            {1161, "Hash128"}
-        };
     }
 
     [Export(typeof(ArchiveFormat))]
@@ -581,18 +379,10 @@ namespace GameRes.Formats.Edoire
             return blocks_stream;
         }
 
-        private string unityVersion = "2.5.0f5";
-        private BuildTarget m_TargetPlatform = BuildTarget.UnknownPlatform;
-        private bool m_EnableTypeTree = true;
-        private List<SerializedType> m_Types;
-        private int bigIDEnabled = 0;
-        private List<ObjectInfo> m_Objects;
-
         private Stream SerializedFile(AssetReader reader)
         {
             // ReadHeader
             var header = new SerializedFileHeader();
-            header = new SerializedFileHeader();
             header.m_MetadataSize = reader.ReadUInt32();
             header.m_FileSize = reader.ReadUInt32();
             header.m_Version = (SerializedFileFormatVersion)reader.ReadUInt32();
@@ -622,92 +412,96 @@ namespace GameRes.Formats.Edoire
             {
                 reader.SetupReaders((int)header.m_Version, true);
             }
+            string unity_version = "2.5.0f5";
             if (header.m_Version >= SerializedFileFormatVersion.Unknown_7)
             {
-                unityVersion = reader.ReadCString();
+                unity_version = reader.ReadCString();
             }
+            BuildTarget target_platform = BuildTarget.UnknownPlatform;
             if (header.m_Version >= SerializedFileFormatVersion.Unknown_8)
             {
-                m_TargetPlatform = (BuildTarget)reader.ReadInt32();
+                target_platform = (BuildTarget)reader.ReadInt32();
             }
+            bool enable_typetree = true;
             if (header.m_Version >= SerializedFileFormatVersion.HasTypeTreeHashes)
             {
-                m_EnableTypeTree = reader.ReadByte() != 0;
+                enable_typetree = reader.ReadByte() != 0;
             }
 
             // Read Types
-            int typeCount = reader.ReadInt32();
-            m_Types = new List<SerializedType>();
-            for (int i = 0; i < typeCount; i++)
+            int type_count = reader.ReadInt32();
+            var types = new List<SerializedType>();
+            for (int i = 0; i < type_count; i++)
             {
-                m_Types.Add(ReadSerializedType(reader, header, false));
+                types.Add(ReadSerializedType(reader, header, enable_typetree));
             }
 
+            int enable_bigid = 0;
             if (header.m_Version >= SerializedFileFormatVersion.Unknown_7 && header.m_Version < SerializedFileFormatVersion.Unknown_14)
             {
-                bigIDEnabled = reader.ReadInt32();
+                enable_bigid = reader.ReadInt32();
             }
 
             // Read Objects
-            int objectCount = reader.ReadInt32();
-            m_Objects = new List<ObjectInfo>();
-            for (int i = 0; i < objectCount; i++)
+            int object_count = reader.ReadInt32();
+            var objects = new List<ObjectInfo>();
+            for (int i = 0; i < object_count; i++)
             {
-                var objectInfo = new ObjectInfo();
-                if (bigIDEnabled != 0)
+                var object_info = new ObjectInfo();
+                if (enable_bigid != 0)
                 {
-                    objectInfo.m_PathID = reader.ReadInt64();
+                    object_info.m_PathID = reader.ReadInt64();
                 }
                 else if (header.m_Version < SerializedFileFormatVersion.Unknown_14)
                 {
-                    objectInfo.m_PathID = reader.ReadInt32();
+                    object_info.m_PathID = reader.ReadInt32();
                 }
                 else
                 {
                     reader.AlignStream(4);
-                    objectInfo.m_PathID = reader.ReadInt64();
+                    object_info.m_PathID = reader.ReadInt64();
                 }
 
                 if (header.m_Version >= SerializedFileFormatVersion.LargeFilesSupport)
-                    objectInfo.byteStart = reader.ReadInt64();
+                    object_info.byteStart = reader.ReadInt64();
                 else
-                    objectInfo.byteStart = reader.ReadUInt32();
+                    object_info.byteStart = reader.ReadUInt32();
 
-                objectInfo.byteStart += header.m_DataOffset;
-                objectInfo.byteSize = reader.ReadUInt32();
-                objectInfo.typeID = reader.ReadInt32();
+                object_info.byteStart += header.m_DataOffset;
+                object_info.byteSize = reader.ReadUInt32();
+                object_info.typeID = reader.ReadInt32();
                 if (header.m_Version < SerializedFileFormatVersion.RefactoredClassId)
                 {
-                    objectInfo.classID = reader.ReadUInt16();
-                    objectInfo.serializedType = m_Types.Find(x => x.classID == objectInfo.typeID);
+                    object_info.classID = reader.ReadUInt16();
+                    object_info.serializedType = types.Find(x => x.classID == object_info.typeID);
                 }
                 else
                 {
-                    var type = m_Types[objectInfo.typeID];
-                    objectInfo.serializedType = type;
-                    objectInfo.classID = type.classID;
+                    var type = types[object_info.typeID];
+                    object_info.serializedType = type;
+                    object_info.classID = type.classID;
                 }
                 if (header.m_Version < SerializedFileFormatVersion.HasScriptTypeIndex)
                 {
-                    objectInfo.isDestroyed = reader.ReadUInt16();
+                    object_info.isDestroyed = reader.ReadUInt16();
                 }
                 if (header.m_Version >= SerializedFileFormatVersion.HasScriptTypeIndex && header.m_Version < SerializedFileFormatVersion.RefactorTypeData)
                 {
-                    var m_ScriptTypeIndex = reader.ReadInt16();
-                    if (objectInfo.serializedType != null)
-                        objectInfo.serializedType.m_ScriptTypeIndex = m_ScriptTypeIndex;
+                    var script_type = reader.ReadInt16();
+                    if (object_info.serializedType != null)
+                        object_info.serializedType.m_ScriptTypeIndex = script_type;
                 }
                 if (header.m_Version == SerializedFileFormatVersion.SupportsStrippedObject || header.m_Version == SerializedFileFormatVersion.RefactoredClassId)
                 {
-                    objectInfo.stripped = reader.ReadByte();
+                    object_info.stripped = reader.ReadByte();
                 }
-                m_Objects.Add(objectInfo);
+                objects.Add(object_info);
             }
 
             Stream data_stream = null;
             if (header.m_FileSize == reader.Source.Length)
             {
-                foreach (var obj in m_Objects)
+                foreach (var obj in objects)
                 {
                     if (obj.classID != 142) //AssetBundle Object以外
                     {
@@ -734,7 +528,7 @@ namespace GameRes.Formats.Edoire
             return data_stream;
         }
 
-        private SerializedType ReadSerializedType(AssetReader reader, SerializedFileHeader header, bool isRefType)
+        private SerializedType ReadSerializedType(AssetReader reader, SerializedFileHeader header, bool enable_typetree)
         {
             var type = new SerializedType();
             type.classID = reader.ReadInt32();
@@ -751,89 +545,44 @@ namespace GameRes.Formats.Edoire
 
             if (header.m_Version >= SerializedFileFormatVersion.HasTypeTreeHashes)
             {
-                if (isRefType && type.m_ScriptTypeIndex >= 0)
-                {
-                    type.m_ScriptID = reader.ReadBytes(16);
-                }
-                else if ((header.m_Version < SerializedFileFormatVersion.RefactoredClassId && type.classID < 0) || (header.m_Version >= SerializedFileFormatVersion.RefactoredClassId && type.classID == 114))
+                if ((header.m_Version < SerializedFileFormatVersion.RefactoredClassId && type.classID < 0) || (header.m_Version >= SerializedFileFormatVersion.RefactoredClassId && type.classID == 114))
                 {
                     type.m_ScriptID = reader.ReadBytes(16);
                 }
                 type.m_OldTypeHash = reader.ReadBytes(16);
             }
 
-            if (m_EnableTypeTree)
+            if (enable_typetree)
             {
                 type.m_Type = new TypeTree();
                 type.m_Type.m_Nodes = new List<TypeTreeNode>();
-                TypeTreeBlobRead(reader, header, type.m_Type);
+                int nodes_count = reader.ReadInt32();
+                int buffer_size = reader.ReadInt32();
+                for (int i = 0; i < nodes_count; i++)
+                {
+                    var typetree_node = new TypeTreeNode();
+                    type.m_Type.m_Nodes.Add(typetree_node);
+                    typetree_node.m_Version = reader.ReadUInt16();
+                    typetree_node.m_Level = reader.ReadByte();
+                    typetree_node.m_TypeFlags = reader.ReadByte();
+                    typetree_node.m_TypeStrOffset = reader.ReadUInt32();
+                    typetree_node.m_NameStrOffset = reader.ReadUInt32();
+                    typetree_node.m_ByteSize = reader.ReadInt32();
+                    typetree_node.m_Index = reader.ReadInt32();
+                    typetree_node.m_MetaFlag = reader.ReadInt32();
+                    if (header.m_Version >= SerializedFileFormatVersion.TypeTreeNodeWithTypeFlags)
+                    {
+                        typetree_node.m_RefTypeHash = reader.ReadUInt64();
+                    }
+                }
+                type.m_Type.m_StringBuffer = reader.ReadBytes(buffer_size);
                 if (header.m_Version >= SerializedFileFormatVersion.StoresTypeDependencies)
                 {
-                    if (isRefType)
-                    {
-                        type.m_ClassName = reader.ReadCString();
-                        type.m_NameSpace = reader.ReadCString();
-                        type.m_AsmName = reader.ReadCString();
-                    }
-                    else
-                    {
-                        type.m_TypeDependencies = reader.ReadInt32Array();
-                    }
+                    type.m_TypeDependencies = reader.ReadInt32Array();
                 }
             }
 
             return type;
-        }
-
-        private void TypeTreeBlobRead(AssetReader reader, SerializedFileHeader header, TypeTree m_Type)
-        {
-            int numberOfNodes = reader.ReadInt32();
-            int stringBufferSize = reader.ReadInt32();
-            for (int i = 0; i < numberOfNodes; i++)
-            {
-                var typeTreeNode = new TypeTreeNode();
-                m_Type.m_Nodes.Add(typeTreeNode);
-                typeTreeNode.m_Version = reader.ReadUInt16();
-                typeTreeNode.m_Level = reader.ReadByte();
-
-                typeTreeNode.m_TypeFlags = reader.ReadByte();
-                typeTreeNode.m_TypeStrOffset = reader.ReadUInt32();
-                typeTreeNode.m_NameStrOffset = reader.ReadUInt32();
-                typeTreeNode.m_ByteSize = reader.ReadInt32();
-                typeTreeNode.m_Index = reader.ReadInt32();
-                typeTreeNode.m_MetaFlag = reader.ReadInt32();
-                if (header.m_Version >= SerializedFileFormatVersion.TypeTreeNodeWithTypeFlags)
-                {
-                    typeTreeNode.m_RefTypeHash = reader.ReadUInt64();
-                }
-            }
-            m_Type.m_StringBuffer = reader.ReadBytes(stringBufferSize);
-
-            using (var stringBufferReader = new BinaryStream(new MemoryStream(m_Type.m_StringBuffer), null))
-            {
-                for (int i = 0; i < numberOfNodes; i++)
-                {
-                    var m_Node = m_Type.m_Nodes[i];
-                    m_Node.m_Type = ReadString(stringBufferReader, m_Node.m_TypeStrOffset);
-                    m_Node.m_Name = ReadString(stringBufferReader, m_Node.m_NameStrOffset);
-                }
-            }
-
-            string ReadString(BinaryStream stringBufferReader, uint value)
-            {
-                var isOffset = (value & 0x80000000) == 0;
-                if (isOffset)
-                {
-                    stringBufferReader.AsStream.Position = value;
-                    return stringBufferReader.ReadCString();
-                }
-                var offset = value & 0x7FFFFFFF;
-                if (CommonString.StringBuffer.TryGetValue(offset, out var str))
-                {
-                    return str;
-                }
-                return offset.ToString();
-            }
         }
     }
 }
